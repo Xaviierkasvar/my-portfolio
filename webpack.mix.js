4f2c597d54ac3bfc435ea733088784349b57bbc0
Francisco Javier Castillo Barrios <javier_castillo_15@hotmail.es>
2024-10-25 05:13:25 -0500

    update changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -38,14 +38,14 @@ mix.webpackConfig({
     ],
 });
 
-mix.copy('resources/assets/common/img/projects', 'public/assets/common/img/projects');
+// Elimina o comenta estas líneas para que Mix no copie automáticamente estas carpetas
+// mix.copy('resources/assets/common/img/projects', 'public/assets/common/img/projects');
+// mix.copy('resources/assets/common/cv', 'public/assets/common/cv');
 
-mix.copy('resources/assets/common/cv', 'public/assets/common/cv');
-
-mix.js('resources/js/client/admin/roots/app.js', 'public/js/client/admin/roots')
-    .js('resources/js/client/frontend/roots/projects.js', 'public/js/client/frontend/roots/projects.js')
-    .js('resources/js/client/frontend/roots/error.js', 'public/js/client/frontend/roots/error.js')
-    .react();
+// mix.js('resources/js/client/admin/roots/app.js', 'public/js/client/admin/roots')
+//     .js('resources/js/client/frontend/roots/projects.js', 'public/js/client/frontend/roots/projects.js')
+//     .js('resources/js/client/frontend/roots/error.js', 'public/js/client/frontend/roots/error.js')
+//     .react();
 
 if (mix.inProduction()) {
     mix.version();
